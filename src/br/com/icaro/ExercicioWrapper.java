362562ccde0346cae32e3fa222a496914f01f8eb
RedSkull_7_7 <icaro.nata147@gmail.com>
2024-08-27 07:57:46 -0300

    first commit

---
+++ src/br/com/icaro/ExercicioWrapper.java
@@ -0,0 +1,13 @@
+package br.com.icaro;
+
+public class ExercicioWrapper {
+
+	public static void main(String[] args) {
+		int idade = 10;
+		System.out.println(idade);
+		
+		long idade1 = idade;
+		System.out.println(idade1);
+	}
+
+}
